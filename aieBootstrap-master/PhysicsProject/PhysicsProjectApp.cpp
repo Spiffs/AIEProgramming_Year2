a1cff8c836ba7b958caa4be99bb1a472403ac745
Oscar <oscarbuzzsmith@gmail.com>
2021-02-11 14:33:37 +1100

    Bootstrap commit 12 | Trigger enter | 11022021

---
+++ aieBootstrap-master/PhysicsProject/PhysicsProjectApp.cpp
@@ -0,0 +1,190 @@
+#include "PhysicsProjectApp.h"
+#include "Sphere.h"
+#include "Plane.h"
+#include "Box.h"
+#include "Spring.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+#include "glm\ext.hpp"
+#include <Gizmos.h>
+
+PhysicsProjectApp::PhysicsProjectApp()
+{
+
+}
+
+PhysicsProjectApp::~PhysicsProjectApp()
+{
+
+}
+
+bool PhysicsProjectApp::startup() {
+	
+	// Increases 2D line coun to maximise the number of objects we can draw.
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
+
+	m_2dRenderer = new aie::Renderer2D();
+
+	// TODO: remember to change this when redistributing a build!
+	// the following path would be used instead: "./font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+	m_physicsScene = new PhysicsScene();
+
+	m_physicsScene->SetGravity(glm::vec2(0, -10));
+
+	// Lower the valu, the more accurate the simulation will be;
+	// but it will increase the processing time required.
+	// If it is too high it causes the sim to stutter and reduce stability.
+	m_physicsScene->SetTimeStep(0.01f);
+
+	/*DrawRect();*/
+	/*SphereAndPlane();*/
+	SpringTest(10);
+
+	return true;
+}
+
+void PhysicsProjectApp::shutdown() {
+
+	delete m_font;
+	delete m_2dRenderer;
+}
+
+void PhysicsProjectApp::update(float deltaTime) {
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+
+	aie::Gizmos::clear();
+
+	m_physicsScene->Update(deltaTime);
+	m_physicsScene->Draw();
+
+	if (input->isMouseButtonDown(0))
+	{
+		int xScreen, yScreen;
+		input->getMouseXY(&xScreen, &yScreen);
+		glm::vec2 worldPos = ScreenToWorld(glm::vec2(xScreen, yScreen));
+		aie::Gizmos::add2DCircle(worldPos, 5, 32, glm::vec4(0.3f));
+	}
+
+
+
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+}
+
+void PhysicsProjectApp::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+
+
+
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	// x-axis = -100 to 100, y-axis = -56.25 to 65.25 (m_extents = 100 / 16:9)
+	aie::Gizmos::draw2D(glm::ortho<float>(-m_extents, m_extents, -m_extents / m_aspectRatio, m_extents / m_aspectRatio, -1.0f, 1.0f));
+
+	// draw your stuff here!
+	
+	char fps[32];
+	sprintf_s(fps, 32, "FPS %i", getFPS());
+	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
+
+	// output some text, uses the last used colour
+	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
+
+glm::vec2 PhysicsProjectApp::ScreenToWorld(glm::vec2 a_screenPos)
+{
+	glm::vec2 worldPos = a_screenPos;
+
+
+	// we will move the center of the screen to (0, 0)
+	worldPos.x -= getWindowWidth() / 2;
+	worldPos.y -= getWindowHeight() / 2;
+
+	// scale this according to the extents
+	worldPos.x *= 2.f * m_extents / getWindowWidth();
+	worldPos.y *= 2.f * m_extents / (m_aspectRatio * getWindowHeight());
+
+
+	return worldPos;
+}
+
+void PhysicsProjectApp::DrawRect()
+{
+	m_physicsScene->AddActor(new Sphere(glm::vec2(20, 10), glm::vec2(-10, -17), 1, 3, glm::vec4(1, 0, 0, 1)));
+	m_physicsScene->AddActor(new Plane(glm::vec2(-0.65, 0.75), -25));
+
+	Box* box1 = new Box(glm::vec2(-20, 0), glm::vec2(16, -4), 1, 4, 8, 4, glm::vec4(1, 0, 0, 1));
+	box1->SetRotation(0.5f);
+
+	Box* box2 = new Box(glm::vec2(10, 0), glm::vec2(-4, 0), 1, 4, 8, 4, glm::vec4(0, 1, 0, 1));
+
+	m_physicsScene->AddActor(box1);
+	m_physicsScene->AddActor(box2);
+
+	box1->ApplyForce(glm::vec2(30, 0), glm::vec2(0));
+	box2->ApplyForce(glm::vec2(-15, 0), glm::vec2(0));
+
+	Sphere* ball = new Sphere(glm::vec2(5, 10), glm::vec2(0), 1, 3, glm::vec4(0, 0, 1, 1));
+	ball->SetRotation(0.5);
+	m_physicsScene->AddActor(ball);
+	ball->SetKinematic(true);
+}
+
+void PhysicsProjectApp::SphereAndPlane()
+{
+	Sphere* ball1;
+	ball1 = new Sphere(glm::vec2(-40, 40), glm::vec2(0), 3.0f, 1, glm::vec4(1, 0, 0, 1));
+	m_physicsScene->AddActor(ball1);
+	ball1->ApplyForce(glm::vec2(50, 30), glm::vec2(0));
+
+	Sphere* ball2;
+	ball2 = new Sphere(glm::vec2(40, 40), glm::vec2(0), 3.0f, 1, glm::vec4(0, 1, 0, 1));
+	m_physicsScene->AddActor(ball2);
+	ball2->ApplyForce(glm::vec2(-50, 30), glm::vec2(0));
+
+	Plane* plane = new Plane();
+	m_physicsScene->AddActor(plane);
+}
+
+void PhysicsProjectApp::SpringTest(int a_amount)
+{
+	Sphere* prev = nullptr;
+	for (int i = 0; i < a_amount; i++)
+	{
+		// This will need to spawn the sphere at the bottom of the previous one,
+		// to make a pendulum that is effected by gravity
+		Sphere* sphere = new Sphere(glm::vec2(i * 3, 30 - i * 5), glm::vec2(0), 10, 2, glm::vec4(0, 0, 1, 1));
+
+		if (i == 0)
+		{
+			sphere->SetKinematic(true);
+		}
+
+		m_physicsScene->AddActor(sphere);
+
+		if (prev)
+		{
+			m_physicsScene->AddActor(new Spring(sphere, prev, 10, 500));
+		}
+
+		prev = sphere;
+	}
+
+	Box* box = new Box(glm::vec2(0, -20), glm::vec2(0), 0.3f, 20, 8, 2);
+	box->SetKinematic(true);
+	m_physicsScene->AddActor(box);
+}
