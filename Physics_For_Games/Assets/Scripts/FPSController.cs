a656701c221e33b88d9380801d07f81a443c3838
Oscar <oscarbuzzsmith@gmail.com>
2021-02-25 16:56:02 +1100

    Bootstrap 22 | started fps game | 25022021

---
+++ Physics_For_Games/Assets/Scripts/FPSController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FPSController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 1000)] float MouseSensitivity;
+
+    public float speed;
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        controller.SimpleMove(transform.forward * vertical * speed * Time.fixedDeltaTime);
+        controller.SimpleMove(transform.right * horizontal * speed * Time.fixedDeltaTime);
+        controller.SimpleMove(transform.up * Time.deltaTime);
+
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up * rotateHorizontal * MouseSensitivity * Time.fixedDeltaTime);
+    }
+}
