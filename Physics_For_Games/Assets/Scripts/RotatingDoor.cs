0b6ec2e3c74f4a4669730766d01d4fee09b28ea5
Oscar <oscarbuzzsmith@gmail.com>
2021-04-14 20:29:05 +1000

    Bootstrap commit 42 | Finished Physics For Games | 14042021

---
+++ Physics_For_Games/Assets/Scripts/RotatingDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingDoor : MonoBehaviour
+{
+    public float angleZ = 0;
+    void FixedUpdate()
+    {
+        angleZ = transform.localRotation.eulerAngles.z;
+        if (angleZ <= 285 && angleZ >= 200)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
